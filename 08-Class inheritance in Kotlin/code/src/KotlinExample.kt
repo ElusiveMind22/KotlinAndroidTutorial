de6ae58e2394d88dcb2ddcc7c23302d520a03ec5
Nguyen Duc Hoang <sunlight4d@gmail.com>
2017-09-24 00:05:22 +0700

    update

--- 08-Class inheritance in Kotlin/code/src/KotlinExample.kt
+++ 08-Class inheritance in Kotlin/code/src/KotlinExample.kt
@@ -8,23 +8,36 @@
 
 package hello
 //class Person constructor(val firstName: String, val lastName: String, var age: Int?){
-class Person(val firstName: String, val lastName: String, var age: Int?){
+open class Person(open val firstName: String, val lastName: String, var age: Int?){
     private var fullName = "$firstName $lastName"
     //initializer blocks
     init {
         println("Initialized with firstName: $this.firstName, lastName: $lastName, age: $age")
     }
-    fun talk(message: String = "") {
-//        println("$fullName says $message")
-        println("$firstName $lastName says $message")
+    open fun talk(message: String = "") {
+        println("$fullName says $message")
     }
 }
 
+class Engineer(fieldOfStudy: String, firstName: String, lastName: String, age: Int?): Person(firstName, lastName, age) {
+    override val firstName = super.firstName.toUpperCase()
+    override fun talk(message: String) {
+        super.talk(message)
+        println("I am an engineer")
+    }
+
+}
+
 fun main(args: Array<String>) {
-//    var person = Person(firstName = "Nguyen", lastName = "Duc Hoang", age = null)
-//    println("Function details. Firstname = ${person.firstName}, lastName = ${person.lastName}")
+    /*
     var person = Person(firstName = "Nguyen", lastName = "Duc Hoang", age = 35)
-    println("Function details. Firstname = ${person.firstName}, lastName = ${person.lastName}, age = ${person.age}")
-    //person.fullName
+    println("Details. Firstname = ${person.firstName}, lastName = ${person.lastName}, age = ${person.age}")
     person.talk("Good morning")
+    */
+    var engineer = Engineer("mechanics", firstName = "Alan", lastName = "Turing", age = 40)
+    engineer.talk("Good afternoon")
+    println("Details. Firstname = ${engineer.firstName}, lastName = ${engineer.lastName}, age = ${engineer.age}")
+
+
+
 }
