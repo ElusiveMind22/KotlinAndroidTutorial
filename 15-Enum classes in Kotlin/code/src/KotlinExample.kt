e45c037b0ac4bfe7df949d48b6dd4a70698ceaf3
Nguyen Duc Hoang <sunlight4d@gmail.com>
2017-09-27 18:30:32 +0700

    update

--- 15-Enum classes in Kotlin/code/src/KotlinExample.kt
+++ 15-Enum classes in Kotlin/code/src/KotlinExample.kt
@@ -7,23 +7,17 @@
  */
 
 package hello
-
 enum class CompassDirection {
     EAST,
     WEST,
     SOUTH,
     NORTH
 }
-
 enum class Color(val red: Int, val green: Int, val blue: Int) {
     RED(255, 0, 0),
     GREEN(0, 255, 0),
     BLUE(0,0,255)
 }
-class Person {
-    var hairColor: Color = Color.BLUE
-}
-//Lam sau cung
 enum class ActionState {
     ACTIVE {
         override fun reverseAction() = INACTIVE
@@ -38,7 +32,7 @@ enum class ActionState {
 fun main(args: Array<String>) {
     println("North enum = ${CompassDirection.NORTH}")
     println(Color.BLUE.toString())
-    println("Color details.Name = ${Color.GREEN.name}, ordinal : ${Color.GREEN.ordinal}") // Giai thich ve ordinal
+    println("Color details.Name = ${Color.GREEN.name}, ordinal : ${Color.GREEN.ordinal}")
     println(Color.valueOf("BLUE"))
 //    println(Color.valueOf("BLUE123"))
     for (colorValue in Color.values()) {
