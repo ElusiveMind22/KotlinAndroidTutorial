000aa56bf24992277b47945a87bcdaaa413ca424
Nguyen Duc Hoang <sunlight4d@gmail.com>
2017-09-29 16:22:24 +0700

    update

---
+++ 18-Lambda Expressions and Anonymous Functions in Kotlin/code/src/KotlinExample.kt
@@ -0,0 +1,15 @@
+
+/**
+ *Mr Nguyen Duc Hoang
+ * Youtube channel: https://www.youtube.com/c/nguyenduchoang
+ * Email: sunlight4d@gmail.com
+ * Lambda Expressions and Anonymous Functions in Kotlin
+ */
+
+package hello
+
+fun main(args: Array<String>) {
+    
+}
+
+
---
+++ 19-Observable properties and storing properties in a Map/code/src/KotlinExample.kt
@@ -0,0 +1,58 @@
+
+/**
+ *Mr Nguyen Duc Hoang
+ * Youtube channel: https://www.youtube.com/c/nguyenduchoang
+ * Email: sunlight4d@gmail.com
+ * Observable properties and storing properties in a Map (Kotlin)
+ */
+
+package hello
+import kotlin.properties.Delegates
+
+class User(name: String) {
+    var currentAddress: String by Delegates.observable("no address") {
+        prop, oldAddress, newAddress -> run {
+            println("Address is changing")
+            println("From $oldAddress -> $newAddress")
+            println("property's name = ${prop.name}")
+        }
+
+    }
+}
+//Storing Properties in a Map = like to "store in a json with key-value pairs"
+//var userObject = mapOf(
+//        "name" to "Duc Hoang",
+//        "age"  to 35
+//)
+
+var userObject = mutableMapOf<String, Any?>("name" to "Duc Hoang",
+        "age"  to 35)
+
+
+class Customer(val mapObject: Map<String, Any?>) {
+    val name: String by mapObject
+    val age: Int     by mapObject
+}
+
+fun main(args: Array<String>) {
+    val user = User("Nguyen Duc Hoang")
+    //Fakes address
+    user.currentAddress = "1234 Penn Avenue\n" +
+                          "Pittsburgh, PA 15206\n" +
+                          "United States"
+    user.currentAddress = "Bach Mai street, Hanoi, Vietnam"
+
+    println("User's details. Name = ${userObject.getValue("name")}. age = ${userObject.getValue("age")}")
+    userObject.set("age", 40)
+    println("User's details. Name = ${userObject.getValue("name")}. age = ${userObject.getValue("age")}")
+
+    val customer = Customer(mapOf(
+            "name" to "John Lasseter",
+            "age"  to 30
+    ))
+    println("Customer's details. Name = ${customer.name}, age = ${customer.age}")
+
+}
+
+
+
