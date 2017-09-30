385a06e591b627c618c34ea9dbd331315add3d5b
Nguyen Duc Hoang <sunlight4d@gmail.com>
2017-10-01 00:32:23 +0700

    update

--- 24-This Expression in Kotlin/code/src/KotlinExample.kt
+++ 24-This Expression in Kotlin/code/src/KotlinExample.kt
@@ -7,9 +7,33 @@
  */
 
 package hello
+class A { // label =  @A
+    inner class B { // label = @B
+        fun Int.foo() { // label = @foo
+            val a = this@A // A's this
+            val b = this@B // B's this
+            println("b = $b")
+
+            val c = this // foo()'s receiver, an Int
+            val c1 = this@foo // foo()'s receiver, an Int
+
+            val funLit = lambda@ fun String.() {
+                val d = this // funLit's receiver
+            }
+
+            val funLit2 = { s: String ->
+                // foo()'s receiver, since enclosing lambda expression
+                // doesn't have any receiver
+                val d1 = this
+            }
+        }
+
+    }
+}
 
 fun main(args: Array<String>) {
-
+    var aObject = A()
+    aObject.B().
 }
 
 
